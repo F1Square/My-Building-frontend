1cacb2dbcfab71183bd53de97685748927b9eb46
Abhi_Bhingradiya <abhib2706@gmail.com>
2026-05-10 18:10:21 +0530

    chore: downgrade react-native-reanimated to v3.19.5 and add plugin for legacy architecture support

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    // Reanimated v3 (legacy arch); must stay last. v4 would require newArchEnabled.
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
